e60b87277b460c74ec7646026fc2addc6130f45e
bitranox <rnowotny1966@gmail.com>
2025-05-01 16:48:38 +0200

    update spirv_llvm_translator compile

--- linux/graphictools/compile_rusticl_staging/00_compile_complete_no_removal.sh
+++ linux/graphictools/compile_rusticl_staging/00_compile_complete_no_removal.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
+git pull
 ./10_create_build_env.sh
 ./15_build_libdrm.sh
 ./20_build_spirv_tools.sh
@@ -9,4 +9,4 @@ set -euo pipefail
 ./50_build_mesa.sh
 ./60_cleanup_icds.sh
 ./70_write_env_profile.sh
-python3 80_check_amd_gpu.py
+./80_check_amd_gpu.py
