cce8db26a424c6523849e79fbb3ae0d6cc0a283f
bitranox <rnowotny1966@gmail.com>
2025-05-01 21:23:51 +0200

    update spirv_llvm_translator compile

--- linux/graphictools/compile_rusticl_staging/40_build_libclc.sh
+++ linux/graphictools/compile_rusticl_staging/40_build_libclc.sh
@@ -27,12 +27,35 @@ log "🛠️ Cleaning up old build directory"
 rm -rf "${ROOT}"
 
 # === Force GCC ===
+#log "🛠️ Forcing GCC as the compiler and setup compiler flags"
+#export CC=gcc
+#export CXX=g++
+#export CFLAGS="-O3 -march=native -mtune=native -flto -fomit-frame-pointer -fPIC"
+#export CXXFLAGS="$CFLAGS"
+#export LDFLAGS="-Wl,-O3 -flto"
+
+# === Force GCC ===
 log "🛠️ Forcing GCC as the compiler and setup compiler flags"
-export CC=gcc
-export CXX=g++
-export CFLAGS="-O3 -march=native -mtune=native -flto -fomit-frame-pointer -fPIC"
+# export CFLAGS="-O3 -march=native -flto -fPIC -fvisibility=hidden -fomit-frame-pointer -DNDEBUG -fprofile-generate"
+export CFLAGS="-O3 -march=native -flto -fPIC -fvisibility=hidden -fomit-frame-pointer -DNDEBUG"
 export CXXFLAGS="$CFLAGS"
-export LDFLAGS="-Wl,-O3 -flto"
+# -O3                   # Enable highest level of optimization (aggressive inlining, loop unrolling, vectorization)
+# -march=native         # Optimize code for the local CPU architecture (may break portability)
+# -flto                 # Enable Link Time Optimization (LTO) for better cross-module optimization
+# -fPIC                 # Generate position-independent code (required for shared libraries)
+# -fvisibility=hidden   # Hide all symbols by default; only explicitly exported ones are visible (improves load time and security)
+# -fomit-frame-pointer  # Omit the frame pointer to free a register (slightly faster, but makes debugging stack traces harder)
+# -DNDEBUG              # Disable debug `assert()` and other debug-only code (used in production builds)
+# -fprofile-generate    # Instrument the program to collect profiling data at runtime (for use with PGO - Profile Guided Optimization)
+
+# export LDFLAGS="-flto -Wl,-O1 -Wl,--as-needed -Wl,--strip-all -shared  -fprofile-generate"
+export LDFLAGS="-flto -Wl,-O1 -Wl,--as-needed -Wl,--strip-all -shared"
+# -flto                  # Enable Link Time Optimization (LTO) during linking for cross-module inlining and better optimization
+# -Wl,-O1                # Pass optimization level 1 to the linker (balance between speed and link-time complexity)
+# -Wl,--as-needed        # Only link shared libraries that are actually used (reduces dependencies and load time)
+# -Wl,--strip-all        # Strip all symbol information from the final binary (smaller size, but no debugging symbols)
+# -shared                # Produce a shared object (.so) instead of an executable
+# -fprofile-generate    # Instrument the program to collect profiling data at runtime (for use with PGO - Profile Guided Optimization)
 
 function build_libclc_only() {
   log "Building libclc for LLVM ${LLVM_VERSION} natively..."
