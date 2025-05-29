c24e572131c00192636fa21d789a6c9c27a3f39c
bitranox <rnowotny1966@gmail.com>
2025-05-30 01:44:43 +0200

    adminscript - boilerplate

--- pythontools/boilerplate_test.sh
+++ pythontools/boilerplate_test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # install lib_bash
 "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../lib/check_and_install_lib_bash.sh"
-source /usr/lib/local/lib_bash/lib_bash.sh
+source /usr/local/lib_bash/lib_bash.sh
 elevate "$@"
 update_caller "$@"
 log "OK"
